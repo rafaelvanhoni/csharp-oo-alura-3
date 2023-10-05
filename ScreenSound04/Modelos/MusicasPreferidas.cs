5264374a869534ec46489e767dddfdeb850018bb
Rafael Vanhoni <rafael.vanhoni@gmail.com>
2023-10-05 07:14:55 -0300

    Criando arquivo JSON
    criando arquivo JSON

---
+++ ScreenSound04/Modelos/MusicasPreferidas.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+
+namespace ScreenSound04.Modelos;
+
+internal class MusicasPreferidas
+{
+
+    public string? Nome { get; set; }
+    public List<Musica> ListaDeMusicasFavoritas { get; }
+
+    public MusicasPreferidas(string nome)
+    {
+        Nome = nome;
+        ListaDeMusicasFavoritas = new List<Musica>();
+    }
+
+    public void AdicionarMusicasFavoritas(Musica musica)
+    {
+        ListaDeMusicasFavoritas.Add(musica);
+    }
+
+    public void ExibirMusicasFavoritas()
+    {
+        Console.WriteLine($"\r\nEssas são as músicas favoritas -> {Nome}");
+        foreach(var musica in ListaDeMusicasFavoritas)
+        {
+            Console.WriteLine($"- {musica.Nome} de {musica.Artista}");
+        }
+    }
+
+    public void GerarArquivoJson()
+    {
+        string json = JsonSerializer.Serialize(new
+        {
+            nome = Nome,
+            musicas = ListaDeMusicasFavoritas
+        });
+
+        string nomeDoArquivo = $"musicas-favoritas-do-{Nome}.json";
+
+        File.WriteAllText(nomeDoArquivo, json);
+        Console.WriteLine($"O arquivo Json foi criado com sucesso! {Path.GetFullPath(nomeDoArquivo)}");
+    }
+}
--- ScreenSound04/Program.cs
+++ ScreenSound04/Program.cs
@@ -12,10 +12,10 @@ using (HttpClient client = new HttpClient())
         //Console.ReadKey();
 
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
-
+        /*
         musicas[1998].ExibirDetalhesDaMusica();
         Console.ReadKey();
-        /*
+        
         LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
         Console.ReadKey();
 
@@ -24,7 +24,6 @@ using (HttpClient client = new HttpClient())
 
         LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "metal");
         Console.ReadKey();
-        */
 
         LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");
         Console.ReadKey();
@@ -32,6 +31,29 @@ using (HttpClient client = new HttpClient())
         LinqFilter.FiltrarMusicasPeloAno(musicas, 1999);
         Console.ReadKey();
 
+        */
+
+        var musicasPreferidasDoRafael = new MusicasPreferidas("Rafael");
+        musicasPreferidasDoRafael.AdicionarMusicasFavoritas(musicas[1]);
+        musicasPreferidasDoRafael.AdicionarMusicasFavoritas(musicas[377]);
+        musicasPreferidasDoRafael.AdicionarMusicasFavoritas(musicas[4]);
+        musicasPreferidasDoRafael.AdicionarMusicasFavoritas(musicas[6]);
+        musicasPreferidasDoRafael.AdicionarMusicasFavoritas(musicas[1467]);
+
+        musicasPreferidasDoRafael.ExibirMusicasFavoritas();
+        Console.ReadKey();
+
+        var musicasPreferidasDoJoao = new MusicasPreferidas("João");
+        musicasPreferidasDoJoao.AdicionarMusicasFavoritas(musicas[500]);
+        musicasPreferidasDoJoao.AdicionarMusicasFavoritas(musicas[637]);
+        musicasPreferidasDoJoao.AdicionarMusicasFavoritas(musicas[428]);
+        musicasPreferidasDoJoao.AdicionarMusicasFavoritas(musicas[13]);
+        musicasPreferidasDoJoao.AdicionarMusicasFavoritas(musicas[71]);
+
+        musicasPreferidasDoJoao.ExibirMusicasFavoritas();
+        Console.ReadKey();
+
+        musicasPreferidasDoRafael.GerarArquivoJson();
 
     }
     catch (Exception ex)
